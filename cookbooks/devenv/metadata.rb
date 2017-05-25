84a952b79dd7861d674bb2b847b892aff36c9200
Don Morrison <don@elskwid.net>
2017-05-25 07:41:53 -0700

    Update dependencies

--- cookbooks/devenv/metadata.rb
+++ cookbooks/devenv/metadata.rb
@@ -6,8 +6,8 @@ description      "Cookbook for dev envs with Vagrant, Chef, and (maybe) Docker"
 version          "0.1.0"
 
 # machine
-depends "apt", "~> 2.6"
-depends "build-essential", "~> 2.0"
+depends "apt", "~> 6.1"
+depends "build-essential", "~> 8.0"
 depends "sudo", "> 1.7.2"
 depends "timezone"
-depends "yum", "~> 3.0"
+depends "yum", "~> 5.0"
